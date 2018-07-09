45228344e179968d459c5f3eae32d8d2afda902d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-09 17:57:48 -0600

    add method for if translator exists and if it translates

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,20 @@
 # YOUR TESTS GO IN HERE!
+
+require "minitest/autorun"
+require "minitest/pride"
+require "pry"
+require "./lib/translator"
+
+class TranslatorTest < Minitest::Test
+
+  def test_it_exists
+  translator = Translator.new
+  assert_instance_of Translator, translator
+  end
+
+  def test_it_translates
+    translator = Translator.new
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world")
+  end 
+
+end
